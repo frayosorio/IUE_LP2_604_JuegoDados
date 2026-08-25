b2ecd15a3bf917c2ee5f5d5ece19dc0adc0e3245
unknown <frayosorio@gmail.com>
2026-08-25 08:51:53 -0500

    Se inicia la interfaz grafica de usuario

---
+++ src/FrmJuego.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class FrmJuego extends JFrame {
+
+
+    public FrmJuego(){
+        setSize(500, 300);
+        setTitle("Juguemos a los dados");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        
+    }
+}
